8afce8996828265482812b65596adf6252cd0153
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-27 18:56:42 +0600

    else-if

---
+++ else-if-condition.js
@@ -0,0 +1,17 @@
+var money = 15;
+var butterBon = 15;
+var biscuit = 20;
+var toast = 25;
+var cake = 30;
+
+if (cake <= money) {
+	console.log("ami cake khamu");
+} else if (toast <= money) {
+	console.log("ami toast khamu");
+} else if (biscuit <= money) {
+	console.log("ami biscuit khamu");
+} else if (butterBon <= money) {
+	console.log("ami butter bon khamu");
+} else {
+	console.log("amar jonne cha better ");
+}
