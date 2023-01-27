812a64f160165924585e648f4557049a9378da95
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-27 13:21:47 +0600

    if-else

---
+++ conditional.js
@@ -0,0 +1,16 @@
+var myMoney = 500;
+var phonePrice = 5000;
+// For one condition use if
+if (myMoney > phonePrice) {
+	// if condition is true this code block will execute
+	console.log("I will buy phone");
+}
+
+// For two condition use if and else
+if (myMoney > phonePrice) {
+	// if condition is true this code block will execute
+	console.log("I will buy phone");
+} else {
+	// if condition is false this code block will execute
+	console.log("I won't buy phone");
+}
