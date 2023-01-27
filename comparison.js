e6556fb532f2599f40f57c6512d1a8ac342fa591
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-27 13:05:34 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,14 @@
+// Comparison Operators
+/*
+greater than: >
+smaller then: <
+equal: ==
+not equal: !=
+greater than or equal: >=
+smaller than or equal: <=
+logical and: &&
+logical or: ||
+*/
+
+// money> money2 && home> field && result>money
+// money> money2 || home> field || result>money
