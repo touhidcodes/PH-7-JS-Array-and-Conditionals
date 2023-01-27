9cceae56dae951a3622a28311bb0079708bc863e
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-27 19:00:03 +0600

    nested-condition

---
+++ nested-condition.js
@@ -0,0 +1,13 @@
+var math = true;
+var geometry = true;
+var straightLine = false;
+
+if ((math = true)) {
+	if ((geometry = true)) {
+		console.log("geometry");
+	} else if ((straightLine = true)) {
+		console.log("straight Line");
+	}
+} else {
+	console.log("Dont math");
+}
